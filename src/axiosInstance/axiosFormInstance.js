feb965cf0ffcf8cfbcb39d00e5918fdfebf6ce92
AniketKumar5155 <your_email@example.com>
2026-07-14 04:52:08 +0530

    fix: axiosFormInstane.js

--- src/axiosInstance/axiosFormInstance.js
+++ src/axiosInstance/axiosFormInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 const axiosFormInstance = axios.create({
-    baseURL: `${BACKEND_URL}/api/forms`,
+    baseURL: `/api/forms`,
     withCredentials: true,
 });
 
